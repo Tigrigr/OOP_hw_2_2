028c7cab50cd7884d68aadb4f0325c9c9e04fa71
mikha <Mikhailov.dzenphone@gmail.com>
2023-05-26 16:02:22 +0300

    Great Hufflepuff Class

---
+++ src/Hufflepuff.java
@@ -0,0 +1,35 @@
+public class Hufflepuff {
+    private int industriousness;
+    private int loyalty;
+    private int honesty;
+
+    public Hufflepuff(int industriousness, int loyalty, int honesty) {
+        this.industriousness = industriousness;
+        this.loyalty = loyalty;
+        this.honesty = honesty;
+    }
+
+    public int getIndustriousness() {
+        return industriousness;
+    }
+
+    public void setIndustriousness(int industriousness) {
+        this.industriousness = industriousness;
+    }
+
+    public int getLoyalty() {
+        return loyalty;
+    }
+
+    public void setLoyalty(int loyalty) {
+        this.loyalty = loyalty;
+    }
+
+    public int getHonesty() {
+        return honesty;
+    }
+
+    public void setHonesty(int honesty) {
+        this.honesty = honesty;
+    }
+}
